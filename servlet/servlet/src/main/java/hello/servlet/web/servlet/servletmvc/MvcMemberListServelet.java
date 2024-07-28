b663ff7beee8daacd4d8a5abc668f24d0d784aa0
이예진 <140812985+316hui@users.noreply.github.com>
2024-07-29 01:27:46 +0900

    session2

--- servlet/servlet/src/main/java/hello/servlet/web/servlet/servletmvc/MvcMemberListServelet.java
+++ servlet/servlet/src/main/java/hello/servlet/web/servlet/servletmvc/MvcMemberListServelet.java
@@ -10,26 +10,20 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
-@WebServlet(name = "mvcMemberSaveServelet", urlPatterns = "/servelet-mvc/members/save")
-public class MvcMemberSaveServelet extends HttpServlet {
-
-    MemberRepository memberRepository = MemberRepository.getInstance();
+@WebServlet(name= "mvcMemberListServelet", urlPatterns = "/servelet-mvc/members")
+public class MvcMemberListServelet extends HttpServlet {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age")); //getParameter는 항상 String
+        List<Member> members = memberRepository.findAll();
 
-        Member member = new Member(username, age);
-        memberRepository.save(member);
+        request.setAttribute("members", members);
 
-        //model에 데이터를 보관한다.
-        request.setAttribute("member", member); //request 에 저장공간이 있음!
-
-        String viewPath = "/WEB-INF/views/save-result.jsp";
+        String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
-
     }
 }
